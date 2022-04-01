0df8ff635b7400e85ca3c6c8c92f17272ab753b8
Tyler <tgrande@my.bcit.ca>
2022-03-31 18:01:01 -0700

    added background colour to body and changed the font colour within body for styling

--- lab-starter/src/app.js
+++ lab-starter/src/app.js
@@ -1,14 +1,22 @@
-const divselect = document.querySelectorAll('.original-container div')
-const copyto = document.querySelector('.copy-container')
+const divselect = document.querySelectorAll(".original-container div");
+const copyto = document.querySelector(".copy-container");
 
-divselect.forEach(element => element.addEventListener('click', (evt) => {
-    let copyselect  = evt.target.parentElement.outerHTML;
+divselect.forEach((element) =>
+  element.addEventListener("click", (evt) => {
+    let copyselect = evt.target.parentElement.outerHTML;
     copyto.insertAdjacentHTML("beforeend", copyselect);
-}))
+  })
+);
 
-const resetButton = document.querySelector('button')
+const clear_but = document.querySelector("button");
 
-resetButton.addEventListener('click', (evt) => {
-    copyto.innerHTML = '';
-})
+clear_but.addEventListener("click", (evt) => {
+  copyto.innerHTML = "";
+});
+
+const body = document.querySelector('body')
+body.style.backgroundColor = '#003B49';
+document.querySelector('body').style.color = '#D6FF4E';
+
+clear_but.style.backgroundColor = '#e7e7e7'
 
